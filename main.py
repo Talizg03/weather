9d04dcc325c6a30821cd21babd31658cb23a3c39
Talizg03 <galtali72@gmail.com>
2024-12-23 21:49:56 +0200

    demo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import streamlit as st
+import seaborn as sns
 st.markdown(""" 
 # Title 
 ## subtitle
@@ -7,3 +9,8 @@ st.markdown("""
 
 """)
 st.radio("which desert is best?",["cake","ice cream","fruits"])
+df=sns.load_dataset("penguins")
+fig,ax=plt.subplot()
+sns.scatterplot(data=df,x="flipper_length_mm",y="bill_length_mm",hue="species",ax=
+ax)
+st.pyplot(fig)
