4f683ce8579d24957e5b3f6fda0f0f40cc40ef0d
Talizg03 <galtali72@gmail.com>
2025-01-10 10:01:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ import streamlit as st
 def get_weather(city_name):
     api_key = 'e6affaa056e1b1f765c4716b941ae4e7'
     url = f"https://api.weatherstack.com/current?access_key={api_key}"
+    url = f"https://api.weatherstack.com/forecast?access_key={api_key}"
     querystring = {"query": city_name,"forecast_days":7}
 
     # Send a GET request to the API
-    response = requests.get(url,params=querystring)
+    current_response = requests.get(url,params=querystring)
+    forecast_response = requests.get(url, params=querystring)
 
     # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the response into JSON format
-        data = response.json()
+    if current_response.status_code == 200 and forecast_response.status_code == 200:
+        # Parse the responses into JSON format
+        current_data = current_response.json()
+        forecast_data = forecast_response.json()
 
-        return data
+        return current_data, forecast_data
     else:
-        # If city is not found or request fails
-        return None
+        # If any of the requests fail
+        return None, None
 
 
 # Streamlit UI for input and display
@@ -36,18 +39,18 @@ def weather_app():
 
     if city_name:
         # Get the weather data for the city
-        weather_data = get_weather(city_name)
-        if weather_data:
-            current = weather_data['current']
-            location = weather_data['location']
-            forecast = weather_data.get('forecast', {})
+        current_data, forecast_data = get_weather(city_name)
+        if current_data and forecast_data:
+           current = current_data['current']
+           location = current_data['location']
+           forecast = forecast_data.get('forecast', {})
              # Display the weather data
-            st.write(f"Weather for: {city_name}")
-            st.write(f"Country:{location['country']}")
-            st.write(f"Time of observation {datetime.now(pytz.timezone(location['timezone_id']))}")
-            st.write(f"Temperature: {current['temperature']}°C")
-            st.write(f"The weather description is:{", ".join(current['weather_descriptions'])}")
-            if forecast:
+           st.write(f"Weather for: {city_name}")
+           st.write(f"Country:{location['country']}")
+           st.write(f"Time of observation {datetime.now(pytz.timezone(location['timezone_id']))}")
+           st.write(f"Temperature: {current['temperature']}°C")
+           st.write(f"The weather description is:{", ".join(current['weather_descriptions'])}")
+           if forecast:
                 st.write(f"7-day Forecast:")
                 for day, data in forecast.items():
                     st.write(f"{day}: {data['temperature']}°C, {', '.join(data['weather_descriptions'])}")
